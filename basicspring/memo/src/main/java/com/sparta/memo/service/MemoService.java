1c371903a8c5ce6b516f2f3e540c34ec4d21b6f1
Cho2unseo <escces00@gmail.com>
2025-09-17 16:04:36 +0900

    0917

---
+++ basicspring/memo/src/main/java/com/sparta/memo/service/MemoService.java
@@ -0,0 +1,48 @@
+package com.sparta.memo.service;
+
+import com.sparta.memo.entity.Memo;
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.repository.MemoRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class MemoService {
+    private final MemoRepository memoRepository;
+
+    public MemoService(MemoRepository memoRepository) {
+        this.memoRepository = memoRepository;
+    }
+
+    public MemoResponseDto createMemo(MemoRequestDto requestDto) {
+        Memo memo = new Memo(requestDto);
+        memoRepository.save(memo);
+        MemoResponseDto responseDto = new MemoResponseDto(memo);
+        return responseDto;
+    }
+
+    public List<MemoResponseDto> getMemos() {
+        return memoRepository.findAllByOrderByModifiedAtDesc().stream().map(MemoResponseDto::new).toList();
+    }
+
+    @Transactional
+    public Long updateMemo(Long id, MemoRequestDto requestDto) {
+        Memo memo = findMemo(id);
+        memo.update(requestDto);
+        return id;
+    }
+
+    public Long deleteMemo(Long id) {
+        Memo memo = findMemo(id);
+        memoRepository.delete(memo);
+        return id;
+    }
+
+    public Memo findMemo(Long id) {
+        return memoRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("선택한 메모는 존재하지 않습니다."));
+    }
+}
---
+++ basicspring/spring-prepare/src/main/java/org/sparta/springprepare/Memo.java
@@ -0,0 +1,21 @@
+package org.sparta.springprepare;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Memo {
+    private final String username;
+    private String contents;
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        Memo memo = new Memo("Robbert");
+        System.out.println(memo.getUsername());
+    }
+}
